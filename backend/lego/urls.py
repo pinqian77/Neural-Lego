f937562c624bf940b4e4dee5adc45414f4a8634f
Jerry <jerry.ye0110@gmail.com>
2022-04-09 09:11:48 +0100

    update url

--- backend/lego/urls.py
+++ backend/lego/urls.py
@@ -8,23 +8,24 @@ from . import views
 #router.register('register', views.register)
 #router.register('project', views.projectPage)
 #router.register('project', views.projectPage)
+
 # 'x/' is defined by frontend, 'views.x' is defined by backend
 # frontend SEND DATA TO 'x/';
 # backend SEND DATA BY 'views.x'
 urlpatterns = [
     path('', views.welcome),
 
-    path('login/', views.login),
-    path('register/', views.register),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name = 'register'),
 
-    path('project/', views.projectPage),
-    path('project/upload/', views.uploadProject),
-    path('project/search/', views.search),
+    path('project/<int:pk>/', views.projectPage, name = 'projectPage'),
+    path('project/upload/<int:pk>/', views.uploadProject,name = 'uploadProject'),
+    path('project/search/<int:pk>/', views.search, name = 'search'),
 
-    path('profile/', views.profilePage),
+    path('profile/<int:pk>/', views.profilePage, name = 'profilePage'),
 
-    path('train/', views.trainPage),
+    path('train/<int:pk>/', views.trainPage, name = 'trainPage'),
 
-    path('canvas/', views.canvasPage),
+    path('canvas/<int:pk>', views.canvasPage, name = 'canvasPage'),
 
 ]
