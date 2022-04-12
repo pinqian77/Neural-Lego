f046ab025c87aeb603a8feb25c4832df064a4605
pinqian77 <pin.qian77@gmail.com>
2022-04-12 02:55:44 +0100

    canvas code ok

--- frontend/src/views/CanvasView.vue
+++ frontend/src/views/CanvasView.vue
@@ -199,7 +199,7 @@
                 <div
                   id="myDiagramDiv"
                   style="
-                    flex-grow: 3;
+                    flex-grow: 4;
                     margin-right: 10px;
                     height: 600px;
                     border: solid 4px #82929b;
@@ -209,11 +209,13 @@
                   id="myDiagramDiv"
                   style="flex-grow: 2; height: 600px; border: solid 4px #82929b"
                 >
-                  <pre class="language-python"> 
-                  <code >
-                    {{canvasData.code}}
-                  </code>
-                  </pre>
+                  <prism-editor
+                    class="my-editor"
+                    readonly="false"
+                    v-model="canvasData.code"
+                    :highlight="highlighter"
+                    line-numbers
+                  ></prism-editor>
                 </div>
               </div>
 
@@ -306,10 +308,24 @@
 <script>
 import axios from "axios";
 import * as go from "../../public/go.js";
-import * as hljs from "../../public/highlight.min.js";
+
+//import Prism Editor
+import { PrismEditor } from "vue-prism-editor";
+import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
+
+// import highlighting library (you can use any library you want just return html string)
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism-twilight.css"; // import syntax highlighting styles
 
 export default {
   name: "CanvasView",
+
+  components: {
+    PrismEditor,
+  },
+
   data() {
     return {
       canvasData: {
@@ -368,15 +384,299 @@ export default {
             },
           ],
         },
-        code: "import torch.nn as nn",
+        code: `import torch import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import argparse
+import time
+import os
+
+#setup training parameters
+parser = argparse.ArgumentParser(description='PyTorch MNIST Training')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--test-batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for testing (default: 128)')
+parser.add_argument('--epochs', type=int, default=5, metavar='N',
+                    help='number of epochs to train')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--model-dir', default='./model-mnist-cnn',
+                    help='directory of model for saving checkpoint')
+parser.add_argument('--load-model', action='store_true', default=False,
+                    help='load model or not')
+    
+args = parser.parse_args(args=[]) 
+    
+if not os.path.exists(args.model_dir):
+    os.makedirs(args.model_dir)
+            
+# Judge cuda is available or not
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+#device = torch.device("cuda" if use_cuda else "cpu")
+device = torch.device("cpu")
+    
+torch.manual_seed(args.seed)
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    
+# Setup data loader
+transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+trainset = datasets.MNIST('data', train=True, download=True,
+                    transform=transform)
+testset = datasets.MNIST('data', train=False,
+                    transform=transform)
+train_loader = torch.utils.data.DataLoader(trainset,batch_size=args.batch_size, shuffle=True,**kwargs)
+test_loader = torch.utils.data.DataLoader(testset,batch_size=args.test_batch_size, shuffle=False, **kwargs)
+    
+    
+# Define CNN
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # in_channels:1  out_channels:32  kernel_size:3  stride:1
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        # in_channels:32  out_channels:64  kernel_size:3  stride:1
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+    
+    
+    
+# Train function
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+            
+        #clear gradients
+        optimizer.zero_grad()
+            
+        #compute loss
+        loss = F.cross_entropy(model(data), target)
+            
+        #get gradients and update
+        loss.backward()
+        optimizer.step()
+    
+# Predict function
+def eval_test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, size_average=False).item()
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(test_loader.dataset)
+    test_accuracy = correct / len(test_loader.dataset)
+    return test_loss, test_accuracy
+    
+# Main function, train the initial model or load the model
+def main():
+    model = Net().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr)
+        
+    if args.load_model:
+        # Load model
+        model.load_state_dict(torch.load(os.path.join(args.model_dir, 'final_model.pt')))
+        trnloss, trnacc = eval_test(model, device, train_loader)
+        tstloss, tstacc = eval_test(model, device, test_loader)
+        print('trn_loss: {:.4f}, trn_acc: {:.2f}%'.format(trnloss, 100. * trnacc), end=', ')
+        print('test_loss: {:.4f}, test_acc: {:.2f}%'.format(tstloss, 100. * tstacc))
+            
+    else:
+        # Train initial model
+        for epoch in range(1, args.epochs + 1):
+            start_time = time.time()
+    
+            #training
+            train(args, model, device, train_loader, optimizer, epoch)
+    
+            #get trnloss and testloss
+            trnloss, trnacc = eval_test(model, device, train_loader)
+            tstloss, tstacc = eval_test(model, device, test_loader)
+    
+            #print trnloss and testloss
+            print('Epoch '+str(epoch)+': '+str(int(time.time()-start_time))+'s', end=', ')
+            print('trn_loss: {:.4f}, trn_acc: {:.2f}%'.format(trnloss, 100. * trnacc), end=', ')
+            print('test_loss: {:.4f}, test_acc: {:.2f}%'.format(tstloss, 100. * tstacc))
+            
+        #save model
+        torch.save(model.state_dict(), os.path.join(args.model_dir, 'final_model.pt'))
+    
+if __name__ == '__main__':
+    main()
+   		 `,
       },
     };
   },
 
-  mounted() {
-    // code hightlignt
-    hljs.highlightAll();
+  methods: {
+    highlighter(code) {
+      return highlight(code, languages.js); // languages.<insert language> to return html with markup
+    },
 
+    // Every time request /canvas/, use this method
+    getData() {
+      axios({
+        method: "get",
+        url: "/canvas/" + localStorage.uid + "/" + localStorage.pid + "/",
+      }).then((res) => {
+        console.log(res.data);
+        if (res.data.status == 200) {
+          // render canvas and code
+          this.renderJson();
+          this.renderCode();
+        } else {
+          alert("canvas loading error!");
+        }
+      });
+    },
+
+    getJson() {
+      axios({
+        method: "get",
+        url:
+          "/canvas/getJson/" + localStorage.uid + "/" + localStorage.pid + "/",
+        responseType: "stream",
+      }).then((res) => {
+        console.log(res.data);
+        if (res.data.status == 200) {
+          this.canvas_data.file = res.data;
+        } else {
+          alert("can not get user's json!");
+        }
+      });
+    },
+
+    getPython() {
+      axios({
+        method: "get",
+        url:
+          "/canvas/getPython/" +
+          localStorage.uid +
+          "/" +
+          localStorage.pid +
+          "/",
+        responseType: "stream",
+      }).then((res) => {
+        console.log(res.data);
+        if (res.data.status == 200) {
+          this.canvas_data.code = res.code;
+        } else {
+          alert("can not get user's python!");
+        }
+      });
+    },
+
+    download() {
+      axios({
+        method: "get",
+        url:
+          "/canvas/download/" + localStorage.uid + "/" + localStorage.pid + "/",
+        responseType: "blob",
+      }).then((res) => {
+        console.log(res.data);
+        if (res.data.status == 200) {
+          let blob = new Blob([res], {
+            type: "application/zip;charset-UTF-8",
+          });
+
+          const url = URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.style.display = "none";
+          a.href = url;
+          a.download = "projectttt.zip";
+          a.click();
+          URL.revokeObjectURL(url);
+        } else {
+          alert("can not get user's json!");
+        }
+      });
+    },
+
+    // Update json first, then send to backend and get python code
+    compile() {
+      // Update json
+      this.updateJson();
+
+      // send json to backend
+      this.canvasData.file = myDiagram.model.toJson();
+
+      let formData = new FormData();
+      formData.append("file", this.canvasData.file);
+      axios({
+        method: "post",
+        url:
+          "/canvas/compile/" + localStorage.uid + "/" + localStorage.pid + "/",
+        data: formData,
+      }).then((res) => {
+        console.log(res.data);
+        // If compile successful 200, backend update database, frontend get data and reload
+        if (res.data.status == "200") {
+          console.log("compile ok!");
+        }
+        // If complie fails 500, frontend alert error
+        else if (res.data.status == "500") {
+          alert("The network model is not valid!");
+        }
+        location.replace("/canvas/");
+      });
+    },
+
+    // Update Json accroding to user's operation
+    updateJson() {
+      document.getElementById("mySavedModel").value = myDiagram.model.toJson();
+      myDiagram.isModified = false;
+    },
+
+    // Render Json on the canvas area
+    renderJson() {
+      myDiagram.model = go.Model.fromJson(
+        document.getElementById("mySavedModel").value
+      );
+    },
+
+    // Render .py on the code area
+    renderCode() {},
+
+    // To make canvas tidy 200
+    layout() {
+      this.updateJson();
+      myDiagram.layoutDiagram(true);
+    },
+
+    // Go to train page 200
+    enterTrain() {
+      location.replace("/train/");
+    },
+  },
+
+  mounted() {
     this.canvasData.file = JSON.stringify(this.canvasData.file);
     const $ = go.GraphObject.make; // for conciseness in defining templates
 
@@ -1012,154 +1312,8 @@ export default {
 
     window.myDiagram = myDiagram;
   },
-
-  methods: {
-    // Every time request /canvas/, use this method
-    getData() {
-      axios({
-        method: "get",
-        url: "/canvas/" + localStorage.uid + "/" + localStorage.pid + "/",
-      }).then((res) => {
-        console.log(res.data);
-        if (res.data.status == 200) {
-          // render canvas and code
-          this.renderJson();
-          this.renderCode();
-        } else {
-          alert("canvas loading error!");
-        }
-      });
-    },
-
-    getJson() {
-      axios({
-        method: "get",
-        url:
-          "/canvas/getJson/" + localStorage.uid + "/" + localStorage.pid + "/",
-        responseType: "stream",
-      }).then((res) => {
-        console.log(res.data);
-        if (res.data.status == 200) {
-          this.canvas_data.file = res.data;
-        } else {
-          alert("can not get user's json!");
-        }
-      });
-    },
-
-    getPython() {
-      axios({
-        method: "get",
-        url:
-          "/canvas/getPython/" +
-          localStorage.uid +
-          "/" +
-          localStorage.pid +
-          "/",
-        responseType: "stream",
-      }).then((res) => {
-        console.log(res.data);
-        if (res.data.status == 200) {
-          this.canvas_data.code = res.code;
-        } else {
-          alert("can not get user's python!");
-        }
-      });
-    },
-
-    download() {
-      axios({
-        method: "get",
-        url:
-          "/canvas/download/" + localStorage.uid + "/" + localStorage.pid + "/",
-        responseType: "blob",
-      }).then((res) => {
-        console.log(res.data);
-        if (res.data.status == 200) {
-          let blob = new Blob([res], {
-            type: "application/zip;charset-UTF-8",
-          });
-
-          const url = URL.createObjectURL(blob);
-          const a = document.createElement("a");
-          a.style.display = "none";
-          a.href = url;
-          a.download = "projectttt.zip";
-          a.click();
-          URL.revokeObjectURL(url);
-        } else {
-          alert("can not get user's json!");
-        }
-      });
-    },
-
-    // Update json first, then send to backend and get python code
-    compile() {
-      // Update json
-      this.updateJson();
-
-      // send json to backend
-      this.canvasData.file = myDiagram.model.toJson();
-
-      let formData = new FormData();
-      formData.append("file", this.canvasData.file);
-      axios({
-        method: "post",
-        url:
-          "/canvas/compile/" + localStorage.uid + "/" + localStorage.pid + "/",
-        data: formData,
-      }).then((res) => {
-        console.log(res.data);
-        // If compile successful 200, backend update database, frontend get data and reload
-        if (res.data.status == "200") {
-          console.log("compile ok!");
-        }
-        // If complie fails 500, frontend alert error
-        else if (res.data.status == "500") {
-          alert("The network model is not valid!");
-        }
-        location.replace("/canvas/");
-      });
-    },
-
-    // Update Json accroding to user's operation
-    updateJson() {
-      document.getElementById("mySavedModel").value = myDiagram.model.toJson();
-      myDiagram.isModified = false;
-    },
-
-    // Render Json on the canvas area
-    renderJson() {
-      myDiagram.model = go.Model.fromJson(
-        document.getElementById("mySavedModel").value
-      );
-    },
-
-    // Render .py on the code area
-    renderCode() {},
-
-    // To make canvas tidy 200
-    layout() {
-      this.updateJson();
-      myDiagram.layoutDiagram(true);
-    },
-
-    // Go to train page 200
-    enterTrain() {
-      location.replace("/train/");
-    },
-  },
 };
 </script>
-
-<style type="text/css">
-* {
-  font-family: Consolas, Monaco, monospace;
-  font-size: 11pt;
-  padding: 0;
-  margin: 0;
-}
-</style>
 
 
 <style scoped>
@@ -1173,3 +1327,23 @@ export default {
 </style>
 <style scoped src="../../new_pages/vendor/fontawesome-free/css/all.min.css"></style>
 <style scoped src="../../new_pages/css/sb-admin-2.min.css"></style>
+
+<style>
+/* required class */
+.my-editor {
+  /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
+  background: #2d2d2d;
+  color: #ccc;
+
+  /* you must provide font-family font-size line-height. Example: */
+  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 5px;
+}
+
+/* optional class for removing the outline */
+.prism-editor__textarea:focus {
+  outline: none;
+}
+</style>
