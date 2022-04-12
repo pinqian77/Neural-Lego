553f692348bbfc36e9497aaa548e34fa4431fa8a
pinqian77 <pin.qian77@gmail.com>
2022-04-12 10:10:19 +0100

    com

--- frontend/src/views/CanvasView.vue
+++ frontend/src/views/CanvasView.vue
@@ -684,9 +684,11 @@ if __name__ == '__main__':
   },
 
   mounted() {
-    this.getJson();
-    // this.updateJson();
-    this.canvasData.file = JSON.stringify(this.canvasData.file);
+    // this.getJson();
+    // // this.updateJson();
+    // this.canvasData.file = JSON.stringify(this.canvasData.file);
+    // this.renderJson();
+    this.compile();
 
     const $ = go.GraphObject.make; // for conciseness in defining templates
     var yellowgrad = $(go.Brush, "Linear", {
